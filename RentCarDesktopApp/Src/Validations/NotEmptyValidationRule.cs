c41b3e0b4e1c7a4f45649539e932ca367fc41d92
Kacper Łukasik <k.lukasik@blunovation.com>
2022-05-30 12:34:32 +0200

    Dodano walidacje

---
+++ RentCarDesktopApp/Src/Validations/NotEmptyValidationRule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+
+namespace RentCarDesktopApp.Validations;
+
+public class NotEmptyValidationRule : ValidationRule {
+    
+    public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+    {
+        if (value.ToString().Equals(String.Empty) || value.ToString().Trim().Equals(String.Empty))
+            return new ValidationResult(false, "Te pole nie może być puste");
+
+        return ValidationResult.ValidResult;
+    }
+    
+}
